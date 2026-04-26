4ef06427c2b55c2255b560b8cfa6ac9f27acfc5d
rohitsharma <rs0043071@gmail.com>
2026-04-26 17:38:41 +0530

    inegrate AI

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const authRouter = require("./routes/auth.routes")
 const cookieParser = require("cookie-parser");
 const cors = require('cors');
+const interviewRouter = require("./routes/interview.routes")
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cors({
 }));
 
 app.use("/api/auth", authRouter);
+app.use("/api/interview", interviewRouter)
 
 
 module.exports = app;
---
+++ Backend/src/routes/interview.routes.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const authMiddleware = require("../middlewares/auth.middleware")
+const interviewController = require("../controllers/interview.controller")
+const upload = require("../middlewares/file.middleware")
+
+const interviewRouter = express.Router()
+
+
+interviewRouter.post("/", authMiddleware.authUser, upload.single("resume"), interviewController.generateInterViewReport)
+
+interviewRouter.get("/report/:interviewId", authMiddleware.authUser, interviewController.getInterviewReportById)
+
+
+interviewRouter.get("/", authMiddleware.authUser, interviewController.getAllInterviewReports)
+
+
+interviewRouter.post("/resume/pdf/:interviewReportId", authMiddleware.authUser, interviewController.generateResumePdf)
+
+
+
+module.exports = interviewRouter
